541107e6e2e8cc2ce11f5cc96eb42d3685238cbd
entingwu <christywu19891221@gmail.com>
2023-02-20 18:55:05 -0800

    update

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,35 +1,29 @@
-import { Button, Input, Form, Dialog } from 'antd-mobile'
+import { useState } from 'react';
 import './index.css';
 
-const initialValues = {
-  username: '',
-  password: 123
-};
-
 const Login = () => {
-  const [form] = Form.useForm();
-  const onSubmit = () => {
-    const values = form.getFieldsValue()
-    Dialog.alert({ 
-      content: JSON.stringify(values),
-    })
+  const [name, setName] = useState('');
+  const [password, setPwd] = useState('');
+
+  const clickHandler = () => {
+    alert("Successfully Login!" + name + ',' + password);
+  }
+
+  const onChangeNameHandler = (e) => {
+    setName(e.target.value);
+  }
+
+  const onChangePasswordHandler = (e) => {
+    setPwd(e.target.value);
   }
 
   return (
     <div className="login">
-      <Form 
-        form={form}
-        layout='horizontal' mode='card' initialValues={initialValues}
-        footer={
-          <Button color="primary" onClick={onSubmit}>Login</Button>
-        }>
-        <Form.Item label='username' name='username'>
-          <Input placeholder='username' clearable />
-        </Form.Item>
-        <Form.Item label='password' name='password'>
-          <Input placeholder='password' clearable type='password'/>
-        </Form.Item>
-      </Form>    
+      {name}
+      {password}
+      <div>User Name: <input onChange={onChangeNameHandler}/></div>
+      <div>Password: <input type="password" onChange={onChangePasswordHandler}/></div>
+      <div><button onClick={clickHandler}>Login</button></div>
     </div>
   );
 }
